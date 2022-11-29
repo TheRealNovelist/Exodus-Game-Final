45733470d49c97de1bcf7693845fbf2adb7bc51d
HiepLe-020108 <106507610+HiepLe-020108@users.noreply.github.com>
2022-11-29 23:36:21 +0700

    UI done for building placement
    If anyone need to ask anything, please do, and if anything need to change, inform me first. Thanks

--- Assets/_Project/Scenes/Hiep/Grid Test/CoinManager.cs
+++ Assets/_Project/Scenes/Hiep/Grid Test/CoinManager.cs
@@ -1,16 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
+using TMPro;
 
 public class CoinManager : MonoBehaviour
 {
     public int CoinAmuont;
+
+    [SerializeField] private TMP_Text moneyAmountText;
     // Start is called before the first frame update
     void Start()
     {
         CoinAmuont = 100;
     }
 
-    
+    private void Update()
+    {
+        moneyAmountText.SetText("Money " + CoinAmuont.ToString());
+    }
 }
