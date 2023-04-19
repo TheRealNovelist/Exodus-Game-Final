ae73342ce169ee8b9a7db698c964db58f7579c87
HiepLe-020108 <hiepchung2002@gmail.com>
2023-04-19 09:33:09 +0700

    Character Cotroller

---
+++ Assets/Plugins/KinematicCharacterController/Walkthrough/1- Player Camera Character Setup/Scene/MyCharacterController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using KinematicCharacterController;
+using UnityEngine;
+
+interface ICharacterController
+{
+    
+}
+public class MyCharacterController : ICharacterController
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
