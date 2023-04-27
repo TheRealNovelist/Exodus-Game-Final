6f8340337fd6084169e0ba76faf6ae0a43d3c211
TheRealNovelist <novelistoftheyear@gmail.com>
2023-04-27 13:56:01 +0700

    move assets

---
+++ Assets/_Project/_Scripts/Gameplay/EnemySystem/Gunner/Gunner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using EnemySystem;
+using UnityEngine;
+
+public class Gunner : BaseEnemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
