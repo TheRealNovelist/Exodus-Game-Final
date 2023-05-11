8e7a8b2ce1a10c12963e6b3b34f878dc593024e5
hamyntran <69077978+hamyntran@users.noreply.github.com>
2023-05-11 11:48:55 +0700

    Update ItemSlot.cs

--- Assets/_Project/_Scripts/Gameplay/Inventory/ItemSlot.cs
+++ Assets/_Project/_Scripts/Gameplay/Inventory/ItemSlot.cs
@@ -9,22 +9,25 @@ using UnityEngine.UI;
 public class ItemSlot : MonoBehaviour
 {
     public Item _item;
-    private Image imageItem;
-    private Image imageFrame;
+    [SerializeField] private Image imageItem;
+    [SerializeField] private Image imageFrame;
     public int amount = 0;
-    private TextMeshProUGUI quantityTMP;
+    [SerializeField]   private TextMeshProUGUI quantityTMP;
     
     [SerializeField] private Color32 lockColor = new Color32(90, 90, 90,225);
     private Color32 unlockColor = new Color32(255, 255, 255,255);
     private Color32 nullColor = new Color32(0, 0, 0,0);
 
 
-    private void Awake()
+    private void OnValidate()
     {
+        imageFrame = transform.GetComponent<Image>(); 
         imageItem = transform.GetChild(0).GetComponent<Image>();
-        imageFrame = transform.GetComponent<Image>();
-        quantityTMP = transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>();
+                quantityTMP = transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>();
+
+
     }
+
 
     public void UpdateSlotUI()
     {
