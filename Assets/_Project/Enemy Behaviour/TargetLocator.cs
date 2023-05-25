fd74acbcf84e4b515b838d3ecf183b59df9276fa
TheRealNovelist <novelistoftheyear@gmail.com>
2023-05-25 16:15:32 +0700

    trying to do something new

---
+++ Assets/_Project/Enemy Behaviour/TargetLocator.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetLocator : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    
+}
