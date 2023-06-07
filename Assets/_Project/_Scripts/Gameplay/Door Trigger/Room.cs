ae6530447ba2094e4acaf003d64def239a7fad9d
hamyntran <nguyentranhamy2002@gmail.com>
2023-06-07 21:43:02 +0700

    Merging

--- Assets/_Project/_Scripts/Gameplay/Door Trigger/Room.cs
+++ Assets/_Project/_Scripts/Gameplay/Door Trigger/Room.cs
@@ -10,24 +10,23 @@ public class Room : MonoBehaviour
     [SerializeField] private List<GameObject> connectedGO = new List<GameObject>();
     private static List<GameObject> previousConnection = new List<GameObject>();
 
-     [SerializeField] private bool disableOnStart;
-    
+    [SerializeField] private bool disableOnStart;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             if (previousConnection.Count > 0)
             {
-               var roomsToDisable = previousConnection.Except(connectedGO);
+                var roomsToDisable = previousConnection.Except(connectedGO);
 
-               foreach (GameObject room in roomsToDisable)
-               {
-                   if (room != gameObject)
-                   {
-                       room.SetActive(false);
-
-                   }
-               }
+                foreach (GameObject room in roomsToDisable)
+                {
+                    if (room != gameObject)
+                    {
+                        room.SetActive(false);
+                    }
+                }
             }
 
             foreach (GameObject room in connectedGO)
@@ -39,5 +38,22 @@ public class Room : MonoBehaviour
         }
     }
 
-
+    private void Start()
+    {
+        if (disableOnStart)
+        {
+            foreach (GameObject c in connectedGO)
+            {
+                if (!c.TryGetComponent(out Room room))
+                {
+                    if (c.activeSelf)
+                    {
+                        c.SetActive(false);
+                    }
+                }
+            }
+            
+            gameObject.SetActive(false);
+        }
+    }
 }
