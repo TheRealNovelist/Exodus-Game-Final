36536b09d7ae1e27b186df2380bad4f1e1b9e634
TheRealNovelist <novelistoftheyear@gmail.com>
2023-06-16 14:34:56 +0700

    Revert Charger to old behaviour

---
+++ Assets/_Project/_Scripts/Enemy System/General/MoveToTarget.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace EnemySystem
+{
+    internal class MoveToTarget : IState
+    {
+        private readonly BaseEnemy _enemy;
+        private readonly NavMeshAgent _navMeshAgent;
+        
+        
+        public MoveToTarget(BaseEnemy enemy, NavMeshAgent navMeshAgent)
+        {
+            _enemy = enemy;
+            _navMeshAgent = navMeshAgent;
+        }
+        
+        public void Update()
+        {
+            _navMeshAgent.SetDestination(_enemy.target.position);
+        }
+
+        public void OnEnter()
+        {
+            _navMeshAgent.enabled = true;
+            
+            //Start walking animation
+            if (_enemy.EnemyAnimator)
+            {
+                _enemy.EnemyAnimator.SetTrigger("MoveToTarget");
+            }
+            
+        }
+
+        public void OnExit()
+        {
+            _navMeshAgent.enabled = false;
+            //Stop walking animation
+        }
+    }
+}
